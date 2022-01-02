376c80ac7b971bc73a8d6389d8230f592864f03d
Robert R. Tucci <tucci@ar-tiste.com>
2022-01-02 14:55:51 -0500

    Update Bounder.py

--- Bounder.py
+++ Bounder.py
@@ -1,9 +1,9 @@
 import numpy as np
+np.set_printoptions(precision=3, floatmode="fixed")
 
 
 class Bounder:
-    def __init__(self, o_y_bar_x, px, e_y_bar_x=None,
-                 monotonicity=False, exogeneity=False, strong_exo=False):
+    def __init__(self, o_y_bar_x, px, e_y_bar_x=None):
         """
         The theory and notation in this class is explained in the chapter
         entitled "Personalized Treatment Effects" of my free open source
@@ -14,8 +14,9 @@ class Bounder:
 
         The ultimate goal of this class is to calculate the bounds for PNS,
         PN and PS, given the probability matrices O_{y|x}, P(x) and E_{y|x}.
+
         O_{y|x} and P(x) are called the Observational Probabilities (these
-        from a survey), whereas E_{y|x} are called the Experimental
+        come from a survey), whereas E_{y|x} are called the Experimental
         Probabilities (these come from a RCT).
 
         Each column of  O_{y|x}, P(x) and E_{y|x} sums to 1, so we chose as
@@ -52,6 +53,8 @@ class Bounder:
         e_y_bar_x : np.array[shape=(2, 2)]
             E_{y|x}
         exogeneity : bool
+        left_bds_e_y_bar_x : np.array[shape=(2, 2)]
+            left (low) bounds for each element of E_{y|x}
         monotonicity : bool
         o00 : float
             O_{0,0}
@@ -71,12 +74,18 @@ class Bounder:
             O_{1|1}
         o_y_bar_x : np.array[shape=(2, 2)]
             O_{y|x}
+        pns3_bds : np.array[shape =(3, 2)]
+            [[PNS_low, PNS_high],
+            [PN_low, PN_high],
+            [PS_low, PS_high]]
         px : float
             P(x)
         px0 : float
             P(x=0)
         px1 : float
-            P(x=1}
+            P(x=1)
+        right_bds_e_y_bar_x : np.array[shape=(2, 2)]
+            right (high) bounds for each element of E_{y|x}
         strong_exo : bool
 
         Parameters
@@ -87,10 +96,6 @@ class Bounder:
             P(x)
         e_y_bar_x : np.array[shape=(2, )], None
             E_{y|x}
-
-        monotonicity : bool
-        exogeneity : bool
-        strong_exo : bool
         """
 
         Bounder.check_prob_vec(px)
@@ -116,14 +121,13 @@ class Bounder:
         self.e1b1 = None
         if e_y_bar_x is not None:
             self.set_exp_probs(e_y_bar_x)
+        self.left_bds_e_y_bar_x = np.zeros(shape=(2, 2))
+        self.right_bds_e_y_bar_x = np.zeros(shape=(2, 2))
+        self.pns3_bds = np.zeros(shape=(3, 2))
 
-        self.exogeneity = exogeneity
-        self.monotonicity = monotonicity
-        self.strong_exo = strong_exo
-        if strong_exo:
-            self.exogeneity = True
-
-        np.set_printoptions(precision=6, floatmode="fixed")
+        self.exogeneity = False
+        self.monotonicity = False
+        self.strong_exo = False
 
     def set_obs_probs(self, o_y_bar_x, px):
         """
@@ -183,7 +187,7 @@ class Bounder:
     @staticmethod
     def check_2d_trans_matrix(mat):
         """
-        Checks the 2x2 transition probability mat is well defined.
+        Checks that the 2x2 transition probability matrix mat is well defined.
 
         Parameters
         ----------
@@ -219,13 +223,27 @@ class Bounder:
         assert (vec <= 1).all()
         assert np.abs(sum(vec) - 1) < 1e-5
 
+    def get_ate(self):
+        """
+        Returns ATE = E_{1|1} - E_{1|0} or None
+
+        Returns
+        -------
+        float, None
+
+        """
+        if self.e_y_bar_x is not None:
+            return self.e1b1 - self.e1b0
+        else:
+            return None
+
     def get_py(self):
         """
         Returns P(y) for y=0,1.
 
         Returns
         -------
-        (float, float)
+        float, float
             P(y=0), P(y=1)
 
         """
@@ -269,24 +287,72 @@ class Bounder:
         """
         return self.o00 + self.o11
 
-    def get_pns3_bds(self):
+    def set_exp_probs_bds(self):
         """
-        Returns bounds for PNS3 = (PNS, N, PS).
+        This method sets the class attributes for the elementwise bounds on
+        the transition probability matrix E_{ y|x}.
 
         Returns
         -------
-        np.array of shape =(3,2)
-            [[PNS_low, PNS_high],
-            [PN_low, PN_high],
-            [PS_low, PS_high]]
+        None
 
         """
 
+        right = self.right_bds_e_y_bar_x
+        left = self.left_bds_e_y_bar_x
+        if not self.monotonicity:
+            left[1, 1] = self.o11
+            right[1, 1] = 1 - self.o10
+            left[1, 0] = self.o01
+            right[1, 0] = 1 - self.o00
+
+            # use if a <= x <= b then 1-b <= 1-x <= 1-a
+            left[0, 1] = self.o10
+            right[0, 1] = 1 - self.o11
+            left[0, 0] = self.o00
+            right[0, 0] = 1 - self.o01
+        else:
+            py0, py1 = self.get_py()
+
+            left[1, 1] = py1
+            right[1, 1] = 1 - self.o10
+            left[1, 0] = self.o01
+            right[1, 0] = py1
+
+            # use if a <= x <= b then 1-b <= 1-x <= 1-a
+            left[0, 1] = self.o10
+            right[0, 1] = py0
+            left[0, 0] = py0
+            right[0, 0] = 1 - self.o01
+
+    def get_exp_probs_bds(self):
+        """
+        Returns left (low) and right (high) bounds of e_y_bar_x
+
+        Returns
+        -------
+        np.array[shape=(2, 2)], np.array[shape=(2, 2)]
+            self.left_bds_e_y_bar_x,  self.right_bds_e_y_bar_x
+
+        """
+        return self.left_bds_e_y_bar_x, self.right_bds_e_y_bar_x
+
+    def set_pns3_bds(self):
+        """
+        Tis method sets the class attribute for the bounds for PNS3 = (PNS,
+        PN, PS).
+
+        Returns
+        -------
+        None
+
+        """
+        if self.strong_exo:
+            self.exogeneity = True
         if self.e_y_bar_x is None:         # no experimental data
             pns_bds = [0, self.get_o_star_star()]
             pn_bds = [0, 1]
             ps_bds = [0, 1]
-            return np.array([pns_bds, pn_bds, ps_bds])
         else:
             py0, py1 = self.get_py()
             e_star_bar_star = self.get_e_star_bar_star()
@@ -399,171 +465,120 @@ class Bounder:
             pns_bds = [pns_left, pns_right]
             pn_bds = [pn_left, pn_right]
             ps_bds = [ps_left, ps_right]
-            return np.array([pns_bds, pn_bds, ps_bds])
 
-    def get_exp_probs_bds(self):
+        self.pns3_bds = np.array([pns_bds, pn_bds, ps_bds])
+
+    def get_pns3_bds(self):
         """
-        Returns elementwise bounds on the transition probability matrix E_{
-        y|x}.
+        Returns PNS3 bounds.
 
         Returns
         -------
-        np.array[shape=(2, 2)], np.array[shape=(2, 2)]
-
-            E_{y|x} lower bound, E_{y|x} upper bound
-
-        """
-        left_bds_e_y_bar_x = np.zeros((2, 2))
-        right_bds_e_y_bar_x = np.zeros((2, 2))
-        if not self.monotonicity:
-            left_bds_e_y_bar_x[1, 1] = self.o11
-            right_bds_e_y_bar_x[1, 1] = 1 - self.o10
-            left_bds_e_y_bar_x[1, 0] = self.o01
-            right_bds_e_y_bar_x[1, 0] = 1 - self.o00
-
-            # use if a <= x <= b then 1-b <= 1-x <= 1-a
-            left_bds_e_y_bar_x[0, 1] = self.o10
-            right_bds_e_y_bar_x[0, 1] = 1 - self.o11
-            left_bds_e_y_bar_x[0, 0] = self.o00
-            right_bds_e_y_bar_x[0, 0] = 1 - self.o01
-        else:
-            py0, py1 = self.get_py()
-
-            left_bds_e_y_bar_x[1, 1] = py1
-            right_bds_e_y_bar_x[1, 1] = 1 - self.o10
-            left_bds_e_y_bar_x[1, 0] = self.o01
-            right_bds_e_y_bar_x[1, 0] = py1
-
-            # use if a <= x <= b then 1-b <= 1-x <= 1-a
-            left_bds_e_y_bar_x[0, 1] = self.o10
-            right_bds_e_y_bar_x[0, 1] = py0
-            left_bds_e_y_bar_x[0, 0] = py0
-            right_bds_e_y_bar_x[0, 0] = 1 - self.o01
-
-        return left_bds_e_y_bar_x, right_bds_e_y_bar_x
-
-    @staticmethod
-    def get_joint_exp_probs_bds(bounder_m, bounder_f):
-        """
-        This method merges the bounds obtained from calling
-        bounder_m.get_exp_probs_bds( ) and bounder_f.get_exp_probs_bds(). It
-        returns the merged joint bounds. It doesn't check whether the joint
-        bounds are consistent.
-
-        Parameters
-        ----------
-        bounder_m : Bounder
-            Bounder object for males.
-        bounder_f : Bounder
-            Bounder object for females.
-
-        Returns
-        -------
-        np.array[shape=(2, 2)], np.array[shape=(2, 2)]
-
-            max(E_{y|x,m} lower bound, E_{y|x,f} lower bound)
-            min(E_{y|x,f} upper bound, E_{y|x,f} upper bound)
+         np.array[shape=(3, 2)]
+            [[PNS_low, PNS_high],
+            [PN_low, PN_high],
+            [PS_low, PS_high]]
 
         """
-        left_m, right_m = bounder_m.get_exp_probs_bds()
-        left_f, right_f = bounder_f.get_exp_probs_bds()
+        return self.pns3_bds
 
-        left = np.maximum(left_m, left_f)  # element-wise max
-        assert left.shape == (2, 2)
-        right = np.minimum(right_m, right_f)  # element-wise min
-        assert right.shape == (2, 2)
-        if (right-left >= 0).all():
-            return left, right
-        else:
-            return None, None
-
-    def print_exp_probs(self):
+    def print_exp_probs(self,  st=""):
         """
         Prints the Experimental probabilities E_{y|x}.
 
+        Parameters
+        ----------
+        st : str
+            st is used for more explicit labeling of the male and female
+            strata.
+
         Returns
         -------
         None
 
         """
-        print("E_{y|x}=\n", self.e_y_bar_x)
+        print("E_{y|x" + st + "}=\n", self.e_y_bar_x)
 
-    def print_obs_probs(self):
+    def print_obs_probs(self,  st=""):
         """
         Prints the Observational Probabilities O_{y|x} and P(x).
 
+        Parameters
+        ----------
+        st : str
+            st is used for more explicit labeling of the male and female
+            strata.
+
         Returns
         -------
         None
 
         """
-        print("O_{y|x}=\n", self.o_y_bar_x)
-        print("P_x=\n", self.px)
+        print("O_{y|x" + st + "}=\n", self.o_y_bar_x)
+        print("P_{x" + st + "}=\n", self.px)
 
-    def print_all_probs(self):
+    def print_all_probs(self,  st=""):
         """
         Calls print_obs_probs() and print_exp_probs()
 
+        Parameters
+        ----------
+        st : str
+            st is used for more explicit labeling of the male and female
+            strata.
+
         Returns
         -------
         None
 
         """
-        self.print_obs_probs()
-        self.print_exp_probs()
+        self.print_obs_probs(st)
+        self.print_exp_probs(st)
 
-    def print_exp_probs_bds(self,
-                            left_bds_e_y_bar_x,
-                            right_bds_e_y_bar_x):
+    def print_exp_probs_bds(self,  st=""):
         """
         Prints left (low) and right (high) bounds for each element of E_{y|x}.
 
         Parameters
         ----------
-        left_bds_e_y_bar_x : np.array[shape=(2, 2)]
-            left (low) bounds for each element of E_{y|x}
-        right_bds_e_y_bar_x : np.array[shape=(2, 2)]
-            right (high) bounds for each element of E_{y|x}
+        st : str
+            st is used for more explicit labeling of the male and female
+            strata.
 
         Returns
         -------
 
         """
-        print("E_{0|0}: %.6f <= %.6f <= %.6f"
-            % (left_bds_e_y_bar_x[0, 0],
-            0 if self.e0b0 is None else self.e0b0,
-            right_bds_e_y_bar_x[0, 0]))
-        print("E_{0|1}: %.6f <= %.6f <= %.6f"
-            % (left_bds_e_y_bar_x[0, 1],
-            0 if self.e0b1 is None else self.e0b1,
-            right_bds_e_y_bar_x[0, 1]))
-        print("E_{1|0}: %.6f <= %.6f <= %.6f"
-            % (left_bds_e_y_bar_x[1, 0],
-            0 if self.e1b0 is None else self.e1b0,
-            right_bds_e_y_bar_x[1, 0]))
-        print("E_{1|1}: %.6f <= %.6f <= %.6f"
-            % (left_bds_e_y_bar_x[1, 1],
-            0 if self.e1b1 is None else self.e1b1,
-            right_bds_e_y_bar_x[1, 1]))
-        
-    @staticmethod
-    def print_pns3_bds(pns3_bds):
+        left = self.left_bds_e_y_bar_x
+        right = self.right_bds_e_y_bar_x
+        mid = self.e_y_bar_x
+        for x in range(2):
+            for y in range(2):
+                print("E_{" + str(y) + "|" + str(x) + st + "}: " +
+                        "%.3f <= %.3f <= %.3f"
+                        % (left[y, x],
+                        mid[y, x],
+                        right[y, x]))
+
+    def print_pns3_bds(self,  st=""):
         """
         Prints bounds on PNS3 = (PNS, PN, PS).
 
         Parameters
         ----------
-        pns3_bds : np.array[shape=(3, 2)]
+        st : str
+            st is used for more explicit labeling of the male and female
+            strata.
 
         Returns
         -------
         None
 
         """
-        
-        print("%.6f <= PNS <= %.6f" % (pns3_bds[0, 0], pns3_bds[0, 1]))
-        print("%.6f <= PN  <= %.6f" % (pns3_bds[1, 0], pns3_bds[1, 1]))
-        print("%.6f <= PS  <= %.6f" % (pns3_bds[2, 0], pns3_bds[2, 1]))
+        for i,  st1 in zip([0, 1, 2], ['PNS', ' PN', ' PS']):
+            print("%.3f" % self.pns3_bds[i, 0]
+                   + " <= " + st1 + st + " <= "
+                   + "%.3f" % self.pns3_bds[i, 1])
 
 
 if __name__ == "__main__":
@@ -575,17 +590,15 @@ if __name__ == "__main__":
         o_y_bar_x_f = np.array([[.3, .73],
                                [.7, .27]])
         px_f = np.array([.3, .7])
-        f = Bounder(o_y_bar_x_f, px_f,
-            e_y_bar_x=e_y_bar_x_f)
-        f.print_all_probs()
+        f = Bounder(o_y_bar_x_f, px_f, e_y_bar_x=e_y_bar_x_f)
+        f.print_all_probs(",f")
         print("---------------------------")
         print("Check exp. data is within bds imposed by obs. data:")
-        left_bds_e_y_bar_x_f, right_bds_e_y_bar_x_f =\
-            f.get_exp_probs_bds()
-        f.print_exp_probs_bds(left_bds_e_y_bar_x_f, right_bds_e_y_bar_x_f)
+        f.set_exp_probs_bds()
+        f.print_exp_probs_bds(",f")
         print("---------------------------")
-        pns3_bds_f = f.get_pns3_bds()
-        Bounder.print_pns3_bds(pns3_bds_f)
+        f.set_pns3_bds()
+        f.print_pns3_bds("_f")
 
         print("MALE--------------------------")
         print("input probabilities obtained from exp. and obs. data:")
@@ -595,16 +608,14 @@ if __name__ == "__main__":
                                 [.7, .7]])
         px_m = np.array([.3, .7])
 
-        m = Bounder(o_y_bar_x_m, px_m,
-            e_y_bar_x=e_y_bar_x_m)
-        m.print_all_probs()
+        m = Bounder(o_y_bar_x_m, px_m, e_y_bar_x=e_y_bar_x_m)
+        m.print_all_probs(",m")
         print("---------------------------")
         print("Check exp. data is within bds imposed by obs. data:")
-        left_bds_e_y_bar_x_m, right_bds_e_y_bar_x_m =\
-            m.get_exp_probs_bds()
-        m.print_exp_probs_bds(left_bds_e_y_bar_x_m, right_bds_e_y_bar_x_m)
+        m.set_exp_probs_bds()
+        m.print_exp_probs_bds(",m")
         print("---------------------------")
-        pns3_bds_m = m.get_pns3_bds()
-        Bounder.print_pns3_bds(pns3_bds_m)
+        m.set_pns3_bds()
+        m.print_pns3_bds("_m")
 
     main()
